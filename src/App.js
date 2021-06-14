fdb1bc7d7d4c77123a306a7350a5c3bbc23aaadf
wlsp <wlsp@outlook.de>
2021-06-14 11:43:52 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import Navigation from './componets/Navigation';
-import MAINBG from './componets/Main.BG';
+// import Navigation from './componets/Navigation';
+// import MAINBG from './componets/Main.BG';
 import Hero from './componets/Hero';
 function App() {
   return (
    <main>
 {/* <Navigation></Navigation>  */}
-<Navigation/>
+{/* <Navigation/> */}
 <Hero />
    </main>
   );
